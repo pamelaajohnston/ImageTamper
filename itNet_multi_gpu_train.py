baaf2caf934d9d66397bd6dc1d22ffdd1ccc841a
Pam Johnston <1609098@rgu.ac.uk>
2018-05-29 14:27:31 +0100

    checkpoint

--- itNet_multi_gpu_train.py
+++ itNet_multi_gpu_train.py
@@ -300,7 +300,7 @@ def train():
         summary_writer.add_summary(summary_str, step)
 
       # Save the model checkpoint periodically.
-      if step % 1000 == 0 or (step + 1) == FLAGS.max_steps:
+      if step % 100 == 0 or (step + 1) == FLAGS.max_steps:
         checkpoint_path = os.path.join(FLAGS.train_dir, 'model.ckpt')
         saver.save(sess, checkpoint_path, global_step=step)
 
